0c87e5bfb01bc6af4b503097a3b94635a5c214f8
guiburon <guillaume.buron@yahoo.fr>
2023-11-04 14:16:24 +0100

    Qtile: tracking autostart and .gitignore

---
+++ qtile/autostart.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# set display resolution and monitor positions
+~/.screenlayout/guiburon-linux_dual-monitor.sh
+
+# draw left and right wallpapers
+feh --bg-fill ~/Pictures/wallpapers/wallpaper_R --bg-fill ~/Pictures/wallpapers/wallpaper_L
+
