a449d6eecde25e76ed32fbcddb6f9646c95c2ea2
guiburon <guillaume.buron@yahoo.fr>
2023-11-09 01:09:51 +0100

    Removed not used Qtile

--- qtile/autostart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# set display resolution and monitor positions
-~/.screenlayout/guiburon-linux_dual-monitor.sh
-
-# draw left and right wallpapers
-feh --bg-fill ~/Pictures/wallpapers/wallpaper_R --bg-fill ~/Pictures/wallpapers/wallpaper_L
-
