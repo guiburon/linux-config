0690adfc69bbc9bd9edb451b45e480508f54d0de
guiburon <guillaume.buron@yahoo.fr>
2024-10-01 17:47:34 +0200

    nvim: changed localleader

--- nvim/lua/config/latex.lua
+++ nvim/lua/config/latex.lua
@@ -26,5 +26,5 @@ let g:vimtex_compiler_method = 'pdflatex'
 
 " Most VimTeX mappings rely on localleader and this can be changed with the
 " following line. The default is usually fine and is the symbol "\".
-let maplocalleader = ","
+let maplocalleader = "\\"
 ]])
