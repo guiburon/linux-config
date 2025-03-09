4ad89093a6fb068fb33f3f5f83d1fb7e37884655
guiburon <guillaume.buron@yahoo.fr>
2025-03-09 20:29:21 +0100

    Qtile: lightDM slick greeter shared wallpaper

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -4,7 +4,7 @@
 ~/.screenlayout/guiburon-laptop_linux.sh
 
 # draw left and right wallpapers
-feh --bg-fill ~/Pictures/wallpapers/wallpaper
+feh --bg-fill /usr/share/backgrounds/guiburon/nh-apluto-wide-9-17-15-final_0.png
 
 # natural scrolling direction for touchpad
 xinput set-prop 12 299 1
