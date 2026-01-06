cb5d62fab28e9ffe5d48bfe33238367d8f1fc9d4
guiburon <guillaume.buron@yahoo.fr>
2026-01-06 11:45:32 +0100

    Locking background change script after pkg update

---
+++ lightdm/update-lock-background.sh
@@ -0,0 +1,1 @@
+sed -i """s#background=/usr/share/endeavouros/backgrounds/endeavouros-wallpaper.png#background=/usr/share/backgrounds/guiburon/nh-apluto-wide-9-17-15-final_0.png#""" /etc/lightdm/slick-greeter.conf
